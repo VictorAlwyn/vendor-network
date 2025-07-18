31a8f701a33c7c0018a1ca4c807f5cad9efefe29
VictorAlwyn <alwyn.tablatin@gmail.com>
2025-07-18 23:10:33 +0800

    fix(eip): fixing eip

--- terraform/eip.tf
+++ terraform/eip.tf
@@ -1,24 +1,12 @@
 resource "aws_eip" "eip_1" {
-    domain = "vpc"
     depends_on = [aws_internet_gateway.igw]
-    
-    lifecycle {
-        ignore_changes = [domain_name]
-    }
-    
     tags = {
         Name = "${var.app_name}-eip-1"
     }
 }
 
 resource "aws_eip" "eip_2" {
-    domain = "vpc"
     depends_on = [aws_internet_gateway.igw]
-    
-    lifecycle {
-        ignore_changes = [domain_name]
-    }
-    
     tags = {
         Name = "${var.app_name}-eip-2"
     }
